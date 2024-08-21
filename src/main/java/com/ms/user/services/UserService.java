0b29bfcb3fae775004a8b4a55d1a6a1069c34423
Thiago Tierre <thiagotierre.lima@gmail.com>
2024-08-20 22:10:39 -0300

    configurações iniciais para microserviço de usuario

---
+++ src/main/java/com/ms/user/services/UserService.java
@@ -0,0 +1,20 @@
+package com.ms.user.services;
+
+import com.ms.user.models.UserModel;
+import com.ms.user.repositories.UserRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    UserRepository repository;
+
+    @Transactional
+    public UserModel save(UserModel userModel) {
+        return repository.save(userModel);
+    }
+
+}
